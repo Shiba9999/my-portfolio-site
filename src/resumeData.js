24b258fe98e998600e8e92885f2c9e4cab9eb28c
shiba <rp.shiba2@gmail.com>
2021-08-25 05:07:28 +0530

    done

---
+++ src/resumeData.js
@@ -0,0 +1,118 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Shiba Prasad Mallick",
+    "role": "Frontend Developer ",
+    "linkedinId":"shiba-prasad-b0223718b",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, read technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/shiba-prasad-b0223718b/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Shiba9999",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"https://twitter.com/shiba995",
+          "className":"fa fa-twitter"
+          // "fa-solid fa-envelope"
+        }
+      ],
+    "aboutme":"I am currently a passout student from The Indira Gandhi Institute Of Technology, Sarang and pursued my B.Tech from here. I am a frontend web developer , currently diving deeper into backend developing. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"shibaprasadmallick0@gmail.com",
+    "education":[
+      {
+        "UniversityName":"Indira Gandhi Institute Of Technology, Sarang",
+        "specialization":"Computer Science Engineering",
+        "MonthOfPassing":"july",
+        "YearOfPassing":"2021",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Ravenshaw Junior College",
+        "specialization":"science",
+        "MonthOfPassing":"april",
+        "YearOfPassing":"2016",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"pepcoding",
+        "specialization":"Front End Web Develoment & Data Structre Algorithm",
+        "MonthOfLeaving":"aug",
+        "YearOfLeaving":"2021",
+        "Achievements":"JavaScript , Reactjs ,  firebase  ,  java  , Express, materialUi ",
+        "duration":"8 months"
+      },
+      {
+        "CompanyName":"Udemy",
+        "specialization":"web Development",
+        "MonthOfLeaving":"Dec",
+        "YearOfLeaving":"2020",
+        "Achievements":"MERN stack web development",
+        "duration":"2 Months"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }, {
+        "skillname":"firebase"
+      },
+      {
+        "skillname":"express"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"MovieApp",
+        "description":"Video Streaming Web App",
+        "imgurl":"images/portfolio/phone.jpg",
+        "link":"https://netflix-clone-fc047.web.app/"
+      },
+      {
+        "name":"Resume Builder",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/resume.png",
+        "link":"https://resume-builder-cb251.web.app/"
+      },
+      {
+        "name":"Shopping WebSite",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/shopping.png",
+        "link":"https://shopping-site-frontend.herokuapp.com/"
+      },
+      {
+        "name":" Reels web app",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/reels.png",
+        "link":"https://insta-reel-app.herokuapp.com/"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"We don't just build websites, we build websites that SELLS",
+        "name":"Christopher Dayagdag"
+      },
+      {
+        "description":"If you think math is hard, try web design.",
+        "name":"Trish Parr"
+      }
+    ]
+  }
+  
+  export default resumeData
